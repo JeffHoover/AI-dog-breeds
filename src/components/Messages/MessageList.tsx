26fcddeb26692e5e37b7c345d0ac4a1b908843ae
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 21:10:28 -0400

    working on MessageList

---
+++ src/components/Messages/MessageList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type Message = {
+  id: string;
+  text: string;
+};
+
+type MessageListProps = {
+  messages: Message[];
+};
+
+const MessageList: React.FC<MessageListProps> = ({ messages }) => {
+  if (messages.length === 0) {
+    return <div>No messages.</div>;
+  }
+
+  return (
+    <ul>
+      {messages.map((msg) => (
+        <li key={msg.id}>{msg.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default MessageList;
