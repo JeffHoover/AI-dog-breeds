6ff88c93431adaf9a438be82d2c4bb2b2cd9b1c2
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 21:39:18 -0400

    index test

---
+++ src/index.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+describe('index.tsx', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="root"></div>';
+  });
+
+  test('renders App without crashing', () => {
+    const root = ReactDOM.createRoot(document.getElementById('root')!);
+    expect(() => root.render(<App />)).not.toThrow();
+  });
+});
