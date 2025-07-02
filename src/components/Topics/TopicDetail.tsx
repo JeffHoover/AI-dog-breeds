785a3882d49d12d122c6fb8ab2f390cc08ee023d
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 20:43:00 -0400

    TopicDetail test passing

---
+++ src/components/Topics/TopicDetail.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type Topic = {
+  id: string;
+  title: string;
+};
+
+type Message = {
+  id: string;
+  text: string;
+};
+
+type TopicDetailProps = {
+  topic: Topic;
+  messages: Message[];
+};
+
+const TopicDetail: React.FC<TopicDetailProps> = ({ topic, messages }) => {
+  return (
+    <div>
+      <h2>{topic.title}</h2>
+      {messages.length === 0 ? (
+        <p>No messages yet.</p>
+      ) : (
+        <ul>
+          {messages.map((msg) => (
+            <li key={msg.id}>{msg.text}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default TopicDetail;
