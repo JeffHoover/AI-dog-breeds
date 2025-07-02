b8063054edabb18bef301a2fea4cf9f38089ea85
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 20:50:37 -0400

    NewTopicForm failing

---
+++ src/components/Topics/NewTopicForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type NewTopicFormProps = {
+  onSubmit: (title: string) => void;
+};
+
+const NewTopicForm: React.FC<NewTopicFormProps> = () => {
+  return <div>Not implemented yet</div>;
+};
+
+export default NewTopicForm;
