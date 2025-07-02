b8ed9633feccb6cf2759951ab35ee4fc0044cf6f
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 22:23:12 -0400

    TopicDetails route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,7 +5,7 @@ import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import TopicPage from '../pages/TopicPage';
 import NotFoundPage from '../pages/NotFoundPage';
-
+import TopicDetail from '../components/Topics/TopicDetail';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -14,6 +14,18 @@ const AppRoutes: React.FC = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/topics" element={<TopicPage topics={[]} />} />
+      <Route
+        path="/topics/:id"
+        element={
+          <TopicDetail
+            topic={{ id: '123', title: 'Topic 123' }}
+            messages={[
+              { id: '1', text: 'This is message one' },
+              { id: '2', text: 'Another message' }
+            ]}
+          />
+        }
+      />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
