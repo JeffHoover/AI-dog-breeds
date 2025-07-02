1fbafb421fe0af334a4a86c184f528f7916d7e68
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 21:24:23 -0400

    homepage

---
+++ src/pages/HomePage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HomePage from './HomePage';
+
+describe('HomePage Component', () => {
+  test('renders welcome message', () => {
+    render(<HomePage />);
+    expect(screen.getByText(/welcome to dog breeds app/i)).toBeInTheDocument();
+  });
+});
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return <h2>Welcome to Dog Breeds App</h2>;
+};
+
+export default HomePage;
