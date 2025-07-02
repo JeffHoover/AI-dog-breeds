bd241fa31cefd35b16eeb775ca63223468a57c44
Jeff Hoover <jhoover@jhoover.com>
2025-07-01 20:39:28 -0400

    topiclist tests passing

--- src/components/Topics/TopicList.tsx
+++ src/components/Topics/TopicList.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const TopicList = () => {
-  return <div>Not implemented yet</div>;
+type Topic = {
+  id: string;
+  title: string;
+};
+
+type TopicListProps = {
+  topics?: Topic[];
+};
+
+const TopicList: React.FC<TopicListProps> = ({ topics }) => {
+  if (!topics) {
+    return <div>Loading topics...</div>;
+  }
+
+  if (topics.length === 0) {
+    return <div>No topics available.</div>;
+  }
+
+  return (
+    <ul>
+      {topics.map((topic) => (
+        <li key={topic.id}>{topic.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default TopicList;
